593074fd4c139aa48ddfe7878231443a0d9aae96
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-01 00:29:24 +0600

    added new component

---
+++ src/components/List/ListToDo/ListToDo.js
@@ -0,0 +1,12 @@
+import classes  from "./ListToDo.module.scss";
+
+
+function ListTodo() {
+  return ( 
+    <div className={classes.ListTodo}>
+      
+    </div>
+  );          
+}
+
+export default ListTodo;          
